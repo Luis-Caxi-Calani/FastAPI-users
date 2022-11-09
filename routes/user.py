186d1aaed04ff7bb6b7dc08c2eecee701417c0e1
jesus20202 <jesdelgado05@gmail.com>
2022-11-09 17:10:50 -0500

    V.2.0.j

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from config.db import conn
 from schemas.user import userEntity,usersEntity
 from models.user import User
+from passlib.hash import sha256_crypt
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
+
 
 user = APIRouter()
 
@@ -12,17 +16,25 @@ def find_all_user():
 @user.post('/users')
 def create_user(user: User):
     new_user = dict(user)
-    print(new_user)
-    return "received"
+    new_user["password"] = sha256_crypt.encrypt(new_user["password"])
+    del new_user["id"]    
+
+    id = conn.local.user.insert_one(new_user).insert_id
+    user = conn.local.user.find_one({"_id":id})
+    return userEntity(user)
 
 @user.get('/users/{id}')
-def find_user():
-        return 
+def find_user(id: str):
+        return userEntity(conn.local.user.find_one({"_id": ObjectId(id)}))
+        
 
 @user.put('/users/{id}')
-def update_user():
-        return 
+def update_user(id : str, user: User):
+        conn.local.user.find_one_and_update(
+                {"_id": ObjectId(id)}, {"$set":dict(user)})
+        return userEntity(conn.local.user.find_one({"_id":ObjectId(id)}))
 
 @user.delete('/users/{id}')
-def delete_user():
-        return 
+def delete_user(id : str):
+        userEntity(conn.local.user.find_one_and_delete({"_id": ObjectId(id)})) 
+        return Response(status_code=HTTP_204_NO_CONTENT)
